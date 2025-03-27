271a6a28284ee123b0c70c0856a83b13517c3d8c
pandaman64 <kointosudesuyo@infoseek.jp>
2025-03-27 21:01:50 +0900

    Normalizing now

--- STLC/Substitution.lean
+++ STLC/Substitution.lean
@@ -34,9 +34,22 @@ theorem scons_succ {α : Type} (t : α) (σ : Nat → α) (x : Nat) : (t .: σ) 
 def Rename := Nat → Nat
 
 @[simp]
-theorem scons_comp {α : Type} (v : Nat) (ρ : Rename) (σ : Nat → α) : (v .: ρ) >>> σ = σ v .: (ρ >>> σ) := by
+theorem scons_comp {α β : Type} (v : α) (ρ : Nat → α) (σ : α → β) : (v .: ρ) >>> σ = σ v .: (ρ >>> σ) := by
   funext x
   cases x <;> rfl
+
+@[simp]
+theorem scons_eq_iff {α : Type} {v₁ v₂ : α} {ρ₁ ρ₂ : Nat → α} : v₁ .: ρ₁ = v₂ .: ρ₂ ↔ v₁ = v₂ ∧ ρ₁ = ρ₂ := by
+  apply Iff.intro
+  . intro h
+    refine ⟨?_, ?_⟩
+    . have := congrFun h 0
+      simpa
+    . funext x
+      have := congrFun h (x + 1)
+      simpa
+  . intro h
+    simp [h]
 
 def shift : Rename := fun x => x + 1
 
@@ -57,19 +70,33 @@ def rename (ρ : Rename) : Term → Term
   | .abs t => .abs (rename (up ρ) t)
   | .app t₁ t₂ => .app (rename ρ t₁) (rename ρ t₂)
 
+@[simp]
+theorem rename_var (ρ : Rename) (x : Nat) : rename ρ (.var x) = .var (ρ x) := rfl
+
+@[simp]
+theorem rename_abs (ρ : Rename) (t : Term) : rename ρ (.abs t) = .abs (rename (up ρ) t) := rfl
+
+@[simp]
+theorem rename_app (ρ : Rename) (t₁ t₂ : Term) : rename ρ (.app t₁ t₂) = .app (rename ρ t₁) (rename ρ t₂) := rfl
+
 theorem rename_id : rename id = id := by
   funext t
   induction t with
   | var x => rfl
-  | abs t ih => simp [rename, up, ih]
-  | app t₁ t₂ ih₁ ih₂ => simp [rename, ih₁, ih₂]
+  | abs t ih => simp [up, ih]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
 
 @[simp]
-theorem Term.rename_rename (ρ₁ ρ₂ : Rename) (t : Term) : rename ρ₂ (rename ρ₁ t) = rename (ρ₁ >>> ρ₂) t := by
+theorem rename_rename (ρ₁ ρ₂ : Rename) (t : Term) : rename ρ₂ (rename ρ₁ t) = rename (ρ₁ >>> ρ₂) t := by
   induction t generalizing ρ₁ ρ₂ with
-  | var x => simp [rename, comp]
-  | abs t ih => simp [rename, ih (up ρ₁) (up ρ₂)]; unfold up; simp;
-  | app t₁ t₂ ih₁ ih₂ => simp [rename, ih₁, ih₂]
+  | var x => simp [comp]
+  | abs t ih => simp [up, ih]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+@[simp]
+theorem rename_rename' (ρ₁ ρ₂ : Rename) : rename ρ₁ >>> rename ρ₂ = rename (ρ₁ >>> ρ₂) := by
+  funext t
+  simp [comp]
 
 def Substitution := Nat → Term
 
@@ -79,22 +106,127 @@ def ren (ρ : Rename) : Substitution := ρ >>> ids
 
 theorem ren_def (ρ : Rename) (x : Nat) : ren ρ x = .var (ρ x) := rfl
 
+-- @[simp]
+-- theorem ids_rename (ρ : Rename) : ids >>> rename ρ = ren ρ := rfl
+
 @[simp]
-theorem ids_rename (ρ : Rename) : ids >>> rename ρ = ren ρ := rfl
+theorem ids_rename (ρ : Rename) : ids >>> rename ρ = ρ >>> ids := rfl
+
+@[simp]
+theorem rename_ids (ρ : Rename) (v : Nat) : rename ρ (ids v) = ids (ρ v) := rfl
 
 @[simp]
 theorem scons_ren_shift : ids 0 .: ren shift = ids := by
   funext x
   cases x <;> rfl
 
-def Term.subst (σ : Substitution) : Term → Term
-  | .var x => σ x
-  | .abs t => .abs (t.subst (ids 0 .: (σ >>> rename shift)))
-  | .app t₁ t₂ => .app (t₁.subst σ) (t₂.subst σ)
+@[simp]
+theorem scons_ren_shift' : ids 0 .: ids >>> rename shift = ids := by
+  simp [←scons_comp]
 
-theorem Term.subst_id : subst ids = id := by
+@[simp]
+theorem ids_shift_ids : ids 0 .: shift >>> ids = ids := by
+  simp [←scons_comp]
+
+def subst (σ : Substitution) : Term → Term
+  | .var x => σ x
+  | .abs t => .abs (subst (ids 0 .: (σ >>> rename shift)) t)
+  | .app t₁ t₂ => .app (subst σ t₁) (subst σ t₂)
+
+@[simp]
+theorem subst_var (σ : Substitution) (x : Nat) : subst σ (.var x) = σ x := rfl
+
+@[simp]
+theorem subst_abs (σ : Substitution) (t : Term) : subst σ (.abs t) = .abs (subst (ids 0 .: (σ >>> rename shift)) t) := rfl
+
+@[simp]
+theorem subst_app (σ : Substitution) (t₁ t₂ : Term) : subst σ (.app t₁ t₂) = .app (subst σ t₁) (subst σ t₂) := rfl
+
+@[simp]
+theorem subst_ids : subst ids = id := by
   funext t
   induction t with
   | var x => rfl
-  | abs t ih => simp [subst, ih]
-  | app t₁ t₂ ih₁ ih₂ => simp [subst, ih₁, ih₂]
+  | abs t ih => simp [ih]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+@[simp]
+theorem ids_subst (σ : Substitution) (x : Nat) : subst σ (ids x) = σ x := rfl
+
+@[simp]
+theorem ids_subst' (σ : Substitution) : ids >>> subst σ = σ := by
+  funext x
+  simp [comp]
+
+theorem rename_subst (ρ : Rename) (t : Term) : rename ρ t = subst (ren ρ) t := by
+  induction t generalizing ρ with
+  | var x => rfl
+  | abs t ih => simp [ren, up, ih]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+@[simp]
+theorem rename_subst_comp (ρ : Rename) (σ : Substitution) (t : Term) : subst σ (rename ρ t) = subst (ρ >>> σ) t := by
+  induction t generalizing ρ σ with
+  | var x => rfl
+  | abs t ih => simp [ih, up]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+@[simp]
+theorem rename_subst_comp' (ρ : Rename) (σ : Substitution) : rename ρ >>> subst σ = subst (ρ >>> σ) := by
+  funext t
+  simp [comp]
+
+@[simp]
+theorem subst_rename_comp (σ : Substitution) (ρ : Rename) (t : Term) : rename ρ (subst σ t) = subst (σ >>> rename ρ) t := by
+  induction t generalizing σ ρ with
+  | var x => rfl
+  | abs t ih => simp [ih, up]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+@[simp]
+theorem subst_rename_comp' (σ : Substitution) (ρ : Rename) : subst σ >>> rename ρ = subst (σ >>> rename ρ) := by
+  funext t
+  simp [comp]
+
+@[simp]
+theorem subst_subst (σ₁ σ₂ : Substitution) (t : Term) : subst σ₂ (subst σ₁ t) = subst (σ₁ >>> subst σ₂) t := by
+  induction t generalizing σ₁ σ₂ with
+  | var x => rfl
+  | abs t ih => simp [ih]
+  | app t₁ t₂ ih₁ ih₂ => simp [ih₁, ih₂]
+
+inductive Step : Term → Term → Prop where
+  | step_beta (t u t' : Term) (eq : t' = subst (u .: ids) t) : Step (.app (.abs t) u) t'
+  | step_appL (t₁ t₂ u : Term) : Step t₁ t₂ → Step (.app t₁ u) (.app t₂ u)
+  | step_appR (t u₁ u₂ : Term) : Step u₁ u₂ → Step (.app t u₁) (.app t u₂)
+  | step_abs (t₁ t₂ : Term) : Step t₁ t₂ → Step (.abs t₁) (.abs t₂)
+
+theorem substitutivity {t₁ t₂ : Term} (h : Step t₁ t₂) (σ : Substitution) : Step (subst σ t₁) (subst σ t₂) := by
+  induction h generalizing σ with
+  | step_beta t u t' eq =>
+    subst t'
+    refine .step_beta _ _ _ ?_
+    -- set_option trace.Meta.Tactic.simp.rewrite true in
+    simp -- Wow!
+  | step_appL t₁ t₂ u h ih =>
+    simp
+    exact .step_appL (subst σ t₁) (subst σ t₂) (subst σ u) (ih σ)
+  | step_appR t u₁ u₂ h ih =>
+    simp
+    exact .step_appR (subst σ t) (subst σ u₁) (subst σ u₂) (ih σ)
+  | step_abs t₁ t₂ h ih =>
+    simp
+    let σ' := ids 0 .: σ >>> rename shift
+    exact .step_abs (subst σ' t₁) (subst σ' t₂) (ih σ')
+
+inductive Ty where
+  | base
+  | arrow (a b : Ty)
+deriving Repr, DecidableEq
+
+def TyCtx := Nat → Ty
+
+inductive HasType : TyCtx → Term → Ty → Prop where
+  | type_var (Γ : TyCtx) (x : Nat) (A : Ty) : Γ x = A → HasType Γ (.var x) A
+  | type_abs (Γ : TyCtx) (t : Term) (A B : Ty) : HasType (A .: Γ) t B → HasType Γ (.abs t) (.arrow A B)
+  | type_app (Γ : TyCtx) (t₁ t₂ : Term) (A B : Ty) : HasType Γ t₁ (.arrow A B) → HasType Γ t₂ A → HasType Γ (.app t₁ t₂) B
